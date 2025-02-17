bc9ddfe0a1e9d678fa1ec8adff33bae1c21bdf21
IEyZens <roncint61@gmail.com>
2025-02-17 10:12:59 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	printf("Taille d'un char: %lu byte(s)\n", sizeof(char));
 	printf("Taille d'un short: %lu byte(s)\n", sizeof(short));
-	       printf("Taille d'un int: %lu byte(s)\n", sizeof(int));
-	       printf("Taille d'un long int: %lu byte(s)\n", sizeof(long int));
-	       printf("Taille d'un long long int: %lu byte(s)\n", sizeof(long long int));
-	       printf("Taille d'un float: %lu byte(s)\n", sizeof(float));
-	       return (0);
+	printf("Taille d'un int: %lu byte(s)\n", sizeof(int));
+	printf("Taille d'un long int: %lu byte(s)\n", sizeof(long int));
+	printf("Taille d'un long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Taille d'un float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
