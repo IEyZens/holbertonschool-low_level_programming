24ceaed662e9cc2afd2036112d3eede13e9ff689
IEyZens <roncint61@gmail.com>
2025-02-17 10:08:29 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main permet d'imprimer une chaine de caracteres
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Taille d'un char: %lu byte(s)\n", sizeof(char));
+	       printf("Taille d'un int: %lu byte(s)\n", sizeof(int));
+	       printf("Taille d'un long int: %lu byte(s)\n", sizeof(long int));
+	       printf("Taille d'un long long int: %lu byte(s)\n", sizeof(long long int));
+	       printf("Taille d'un float: %lu byte(s)\n", sizeof(float));
+	       return (0);
+}
