b2dc6e0a5c3239f2a3f8c2113091d8d7f816becf
IEyZens <roncint61@gmail.com>
2025-02-17 09:45:12 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - main permet d'imprimer une chaine de caracteres
+ * Return: 0
+ */
 
-int main(void)
+int  main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
