2b77c7a1f8797b12cdfdadd5afeffc6589d6a55f
IEyZens <roncint61@gmail.com>
2025-02-17 09:49:00 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main permet d'imprimer une chaine de caracteres
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
