7d9127a96a9f24c88702b3c1f0a533ccdd6c79ac
IEyZens <roncint61@gmail.com>
2025-02-18 08:13:58 +0000

    print without two letter

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main permet d'imprimer une chaine de caracteres
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z' && letter != 'q' && 'e'; letter++)
+		putchar(letter);
+
+	putchar ('\n');
+
+	return (0);
+}
