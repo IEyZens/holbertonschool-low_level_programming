694187e9e5dd9d7ab7f1847a653a5cb38f7e2bfb
IEyZens <roncint61@gmail.com>
2025-02-18 08:16:55 +0000

    print without two letter

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z' && letter != 'q' && 'e'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'q' && letter != 'e')
+		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 	}
 
