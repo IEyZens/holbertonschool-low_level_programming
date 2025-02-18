58e5f4088e20324c94d8d32d914f46423aa9fa9d
IEyZens <roncint61@gmail.com>
2025-02-18 09:37:16 +0000

    print comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,12 +9,16 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (num <= 9)
-			putchar(',');
 		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 
 	return (0);
 }
