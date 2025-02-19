c0fbf28d98380520ed8457c055eb5453d21c2606
IEyZens <roncint61@gmail.com>
2025-02-19 10:03:28 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @int: int est un integer
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (1);
+	else
+		return (0);
+}
