3619b8b599a56fc9dc00017bf162fd37a89e133b
IEyZens <roncint61@gmail.com>
2025-02-19 09:15:33 +0000

    _islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - check the code.
+ * @c: est un int
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c == 'c')
+		return (1);
+	else
+		return (0);
+}
