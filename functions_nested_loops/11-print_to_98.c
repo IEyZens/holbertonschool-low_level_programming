456b81cb695048d6e600b46536c6498779a803a1
IEyZens <roncint61@gmail.com>
2025-02-20 12:33:01 +0000

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,11 +9,8 @@ void print_to_98(int n)
 {
 	for (n = 0; n <= 98; n++)
 	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		if (n != 98)
+			printf(", ");
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
