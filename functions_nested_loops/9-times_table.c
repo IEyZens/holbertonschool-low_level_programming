5f94c572298dd48e2a2cb9e93e79925ff9503649
IEyZens <roncint61@gmail.com>
2025-02-20 11:48:59 +0000

    times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,8 +7,18 @@
 
 void times_table(void)
 {
-	int i;
+	int a, b, resultat;
 
-	for (i = 0; i <= 81; i *= 2)
-		_putchar(i);
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+
+		for (b = 1; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			resultat = a * b;
+		}
+	}
 }
