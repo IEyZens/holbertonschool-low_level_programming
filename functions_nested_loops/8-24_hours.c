eaf1b4a7dd12a956b525207b3c052f97f0355b3e
IEyZens <roncint61@gmail.com>
2025-02-20 07:43:34 +0000

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,18 +1,23 @@
 #include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 void jack_bauer(void)
 {
-  int minute, heure;
+	int minute, heure;
 
-  for (heure = 0; heure <= 23; heure++)
-  {
-	  for (minute = 0; minute <= 59; minute++)
-    {
-      _putchar('0');
-      _putchar('0');
-      _putchar(':');
-      _putchar('0');
-      _putchar('0');
-    }
-  }
+	for (heure = 0; heure <= 23; heure++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar('0');
+			_putchar('0');
+			_putchar(':');
+			_putchar('0');
+			_putchar('0');
+		}
+	}
 }
