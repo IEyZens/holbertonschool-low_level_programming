337368b43a842b27b4e4a8737def98b9147fae78
IEyZens <roncint61@gmail.com>
2025-02-24 13:45:15 +0000

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 100; a++)
+	{
+		if ((a % 3) == 0)
+			printf("Fizz");
+		else if ((a % 5) == 0)
+			printf("Buzz");
+		else if ((a % 15) == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", a);
+
+		if (a == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
