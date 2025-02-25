bc8d90e4db918434bdec82b27d293d91298a03cc
IEyZens <roncint61@gmail.com>
2025-02-25 14:01:22 +0000

    print_reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,10 @@ void print_rev(char *s)
 	int i;
 	int len;
 
-	while (*s[len] != '\0')
+	while (s[len] != '\0')
 		len++;
 
 	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
+		_putchar(s[i - 1]);
 	_putchar('\n');
 }
