8c712629bdd6e2f5c5eb9b0767585b9be71e99d9
IEyZens <roncint61@gmail.com>
2025-02-26 08:58:19 +0000

    rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * rev_string - check the code
+ *
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int a = 0, b = 0;
+	char rev;
+
+	while (s[b++])
+		a++;
+
+	for (b = a - 1; b >= a / 2; b--)
+	{
+		rev = s[b];
+		s[b] = s[a - b - 1];
+		s[a - b - 1] = rev;
+	}
+}
