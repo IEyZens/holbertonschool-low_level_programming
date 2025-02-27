be34a4f934cb33ccb73d63b21736ec5d301875be
IEyZens <roncint61@gmail.com>
2025-02-27 12:42:16 +0000

    puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - check the code
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int length = 0, i, index;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2)
+		index = length / 2;
+	else
+		index = (length + 1) / 2;
+
+	for (i = index; i <= length - 1; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
