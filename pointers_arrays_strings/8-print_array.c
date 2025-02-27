52250495a4898a1718b99dc82f953025ade4bd89
IEyZens <roncint61@gmail.com>
2025-02-27 23:25:40 +0000

    print_array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -16,8 +16,10 @@ void print_array(int *a, int n)
 	for (i = 0; i <= n - 1; i++)
 	{
 		printf("%d", a[i]);
-		if (i == n - 1)
-			printf(", ");
+
+		if (i < n - 1)
+			continue;
+		printf(", ");
 	}
 
 	printf("\n");
