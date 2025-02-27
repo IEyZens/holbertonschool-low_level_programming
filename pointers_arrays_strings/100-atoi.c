b68649a7777b7202891050db4297aff7f32e84a0
IEyZens <roncint61@gmail.com>
2025-02-27 14:05:08 +0000

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _atoi - check the code
+ * @s: est un char pointeur
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	int a = 0, b = 1, c;
+
+	while (*s++)
+	{
+		a++;
+		if (b == '-')
+			_putchar('+');
+		else if (b == '+')
+			continue;
+	}
+
+	while (a >= 48 && a <= 57)
+	{
+		c = (a / 10) - '0';
+		if (a > (INT_MAX - c) - 10)
+			continue;
+	}
+
+	return (c * b);
+}
