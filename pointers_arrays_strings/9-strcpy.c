03266fb16734d21cc3464f01ac61bb10ef4d7a3e
IEyZens <roncint61@gmail.com>
2025-02-27 13:40:43 +0000

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	dest[index] = '\0';
+
+	return (dest);
+}
