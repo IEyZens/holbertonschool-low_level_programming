2fcc984f85b76bc5ac3d85412be3729874d7e414
IEyZens <roncint61@gmail.com>
2025-02-28 10:22:10 +0000

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	char *start = dest;
+
+	/* Trouver la fin de la chaine de caracteres */
+	while (*dest != '\0')
+		dest++;
+
+	/* Copier au maximum n caracteres de src dans dest */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		*dest = src[i]; /* Copie du caractere */
+		dest++; /* Avance dans dest */
+	}
+
+	/* Ajout du caractere null a la fin */
+	*dest = '\0';
+
+	return (start); /* Retourne le debut de la chaine modifiee */
+}
