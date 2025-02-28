214fdd5c7ed58b95595d63ddda731a3ed3883b0a
IEyZens <roncint61@gmail.com>
2025-02-28 12:15:53 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
  * _strncpy - check the code
- *
+ * @dest: est un char pointeur
+ * @src: est un char pointeur
+ * @n: est un int
  * Return: Always 0.
  */
 
@@ -27,5 +29,5 @@ char *_strncpy(char *dest, char *src, int n)
 		n--;
 	}
 
-	return start;
+	return (start);
 }
