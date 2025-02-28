b20c150cdb0968f00aa4364b386e15a3648c4f46
IEyZens <roncint61@gmail.com>
2025-02-28 20:36:43 +0100

    string_toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,13 @@
 char *string_toupper(char *str)
 {
 	int i = 0;
-	char c;
 
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
-			return (c - 32);
-		str[i]++;
+			str[i] = str[i] - 32;
+		i++;
 	}
 
-	return (str[i]);
+	return (str);
 }
