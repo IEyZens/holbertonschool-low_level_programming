cb18eee35d30fc96593fd045777f4e3bb3d46844
IEyZens <roncint61@gmail.com>
2025-03-03 12:45:58 +0100

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - check the code
+ *@s: est un char pointeur
+ @accept: est un char pointeur
+ * Return: Always 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (s[c] == accept[i])
+				break;
+		}
+
+		if (accept[i] == '\0')
+			break;
+
+		c++;
+	}
+
+	return (c);
+}
