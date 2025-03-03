a9f0b1bcb82a8330380e09d70738b587001407f4
IEyZens <roncint61@gmail.com>
2025-03-03 11:52:41 +0100

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - prints buffer in hexa
+ * s: est un char pointeur
+ * b: est un char
+ * n: est un int non assigné
+ *
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+
+	return (s);
+}
