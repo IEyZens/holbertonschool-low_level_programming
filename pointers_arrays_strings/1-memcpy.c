765f8397fdde01a643281e4f4d6b32ecf57e82a4
IEyZens <roncint61@gmail.com>
2025-03-03 12:08:04 +0100

    memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @dest: est un char pointeur
+ * @src: est un char pointeur
+ * @n: est un int non assigné
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
