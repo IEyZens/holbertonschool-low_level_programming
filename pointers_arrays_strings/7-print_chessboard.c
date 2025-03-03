e05ddd3db3463b77fb76f5552151b67a3e1f697d
IEyZens <roncint61@gmail.com>
2025-03-03 14:14:19 +0100

    print_chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int l;
+	int c;
+
+	for (l = 0; l < 8; l++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			printf("%c", a[l][c]);
+		}
+
+		printf("\n");
+	}
+}
