85e05249193353d29fa9aeba0623f0f68c2c3ae0
IEyZens <roncint61@gmail.com>
2025-03-04 10:33:08 +0100

    strlen_recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ * _strlen_recursion - check the code
+ *@s: est un char pointeur
  * Return: Always 0.
  */
 
 int _strlen_recursion(char *s)
 {
 	if (s == NULL || *s == '\0')
+	{
 		return (0);
+	}
 	else
 	{
 		return (1 + _strlen_recursion(s + 1));
