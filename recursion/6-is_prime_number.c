2a6619ef3e91bad9e011238456d7450ce153c516
IEyZens <roncint61@gmail.com>
2025-03-04 12:29:39 +0100

    is_prime_number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -5,18 +5,19 @@
  * Return: Always 0.
  */
 
+int i = 2;
+
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 		return (0);
 
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+	if (i * i > n)
+		return (1);
 
-	return (1);
+	if (n % i == 0)
+		return (0);
+
+	i++;
+	return (is_prime_number(n));
 }
