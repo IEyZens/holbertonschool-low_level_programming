e73ef05eeba411e0e5da2cbc4acf1635e1821284
IEyZens <roncint61@gmail.com>
2025-03-05 10:49:11 +0100

    whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h" /* Inclusion de la bibliothèque "main.h" */
+/**
+ * main - Prints the name of the program
+ * @argc: Argument count (not used)
+ * @argv: Argument vector (contains program name)
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	/* Affiche le nom du programme (incluant le chemin si présent) */
+	printf("%s\n", argv[0]);
+
+	/* Retourne 0 pour indiquer une exécution réussie */
+	return (0);
+}
