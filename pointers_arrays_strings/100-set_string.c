9e4a3f315103c205d7f7a2021ce0393378362a05
IEyZens <roncint61@gmail.com>
2025-03-06 14:55:16 +0100

    set_string

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,13 @@
 #include "main.h"
+/**
+ * @brief Set the string object
+ *
+ * @param s
+ * @param to
+ */
 
 void set_string(char **s, char *to)
 {
+	/* La valeur de **s a été modifié pour qu'il pointe vers *to */
 	*s = to;
 }
