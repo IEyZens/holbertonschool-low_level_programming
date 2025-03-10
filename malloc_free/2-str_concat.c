ef44dcde13d61d2263190a7fcc25cf191ca1ff2f
IEyZens <roncint61@gmail.com>
2025-03-10 13:12:49 +0100

    str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,27 +6,30 @@
  * Return: valid the code
  */
 
-char *str_concat(char *s1, char *s2)
-{
-	int size = 0;
-	char *c;
+ char *str_concat(char *s1, char *s2)
+ {
+	 char *concat_str;
+	 int index, concat_index = 0, len = 0;
 
-	if (s1 == NULL)
-		s1 = "";
+	 if (s1 == NULL)
+		 s1 = "";
 
-	if (s2 == NULL)
-		s2 = "";
+	 if (s2 == NULL)
+		 s2 = "";
 
-	size = strlen(s1) + strlen(s2);
+	 for (index = 0; s1[index] || s2[index]; index++)
+		 len++;
 
-	c = (char *)malloc((size + 1) * sizeof(char));
+	 concat_str = malloc(sizeof(char) * len);
 
-	if (!c)
-		return (NULL);
+	 if (concat_str == NULL)
+		 return (NULL);
 
-	strcpy(c, s1);
+	 for (index = 0; s1[index]; index++)
+		 concat_str[concat_index++] = s1[index];
 
-	strcat(c, s2);
+	 for (index = 0; s2[index]; index++)
+		 concat_str[concat_index++] = s2[index];
 
-	return (c);
-}
+	 return (concat_str);
+ }
