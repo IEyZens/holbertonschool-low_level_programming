2c5d6859a318cb0926dd139fe015a53d2d67ed89
IEyZens <roncint61@gmail.com>
2025-03-10 11:25:03 +0100

    create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,22 +4,24 @@
  * @size: est un int non assigné
  * @c: est un char
  *
- * Return 0 - valid the code
+ * Return: valid the code
  */
 
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char* array;
+	char *array;
 
+	/* Allocation de mémoire */
 	array = (char *)malloc(size * sizeof(char));
+	/* Vérification de malloc */
 	if (!array)
 		return (NULL);
 
+	/* Initialisation du tableau */
 	for (i = 0; i < size; i++)
-	{
 		array[i] = c;
-	}
 
+	/* Retourne le pointeur vers le tableau */
 	return (array);
 }
