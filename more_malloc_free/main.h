085ae364c3e187d78e382cbb5d0c0ab1b7c99579
IEyZens <roncint61@gmail.com>
2025-03-11 23:39:56 +0100

    main.h

---
+++ more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
+#include <stddef.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+
+#endif /* MAIN_H */
