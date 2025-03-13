4da76af471ba6c5b38e6d988388d976125512dc3
IEyZens <roncint61@gmail.com>
2025-03-13 14:58:57 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
+#include <stddef.h>
+
 /**
  * struct dog - check the code
  * @name: est un char pointeur
@@ -14,5 +23,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
