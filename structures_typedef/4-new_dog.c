e5083becf6473f295923f76a389613ce7773d116
IEyZens <roncint61@gmail.com>
2025-03-14 15:27:08 +0100

    new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -77,19 +77,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (ptr);
 }
-
-/**
- * free_dog - check the code
- * @d: est un struct dog_t pointeur
- *
- */
-
-void free_dog(dog_t *d)
-{
-	if (d == NULL)
-		return;
-
-	free(d->name);
-	free(d->owner);
-	free(d);
-}
