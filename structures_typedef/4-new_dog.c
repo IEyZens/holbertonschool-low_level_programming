1a3dda6a51ea496dfa464fb59aaaf3ff44608340
IEyZens <roncint61@gmail.com>
2025-03-14 14:51:46 +0100

    new_dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+/**
+ * new_dog - check the code
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *a, *b;
+	dog_t *ptr;
+
+	ptr = (dog_t *)malloc(sizeof(dog_t));
+
+	if (!ptr)
+		return (NULL);
+
+	a = (char *)malloc(strlen(name) + 1);
+
+	if (!a)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	b = (char *)malloc(strlen(owner) + 1);
+
+	if (!b)
+	{
+		free(ptr);
+		free(a);
+		return (NULL);
+	}
+
+	strcpy (a, name);
+	strcpy (b, owner);
+
+	ptr->name = a;
+	ptr->age = age;
+	ptr->owner = b;
+
+	return (ptr);
+}
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
