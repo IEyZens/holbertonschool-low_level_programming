f602b06ac99e621d871fa8789dff201d3d589f56
IEyZens <roncint61@gmail.com>
2025-03-18 00:19:43 +0100

    get_op_func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,20 +1,22 @@
 #include "3-calc.h"
 /**
- * @brief
- *
+ * get_op_func - check the code
+ * @s: est un char pointeur fonction
+ * Return: valid the code
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
