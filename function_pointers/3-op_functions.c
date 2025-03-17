24e12679c2cad5d367e36aec5ddc14b7f4cd7c4e
IEyZens <roncint61@gmail.com>
2025-03-18 00:19:52 +0100

    op_functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,30 +1,124 @@
 #include "3-calc.h"
 
+/**
+ * op_add - +
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
+
 int op_add(int a, int b);
+
+/**
+ * op_sub - -
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
+
 int op_sub(int a, int b);
+
+/**
+ * op_mul - *
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
+
 int op_mul(int a, int b);
+
+/**
+ * op_div - /
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
+
 int op_div(int a, int b);
+
+/**
+ * op_mod - %
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
+
 int op_mod(int a, int b);
+
+/**
+ * op_add - +
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - -
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - *
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
 
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - /
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
+/**
+ * op_mod - %
+ *
+ * @a: est un int
+ * @b: est un int
+ *
+ * Return: valid the code
+ */
 
 int op_mod(int a, int b)
 {
