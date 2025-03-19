28b0ec2e3032b0d8660e49ef370d45d13572515c
IEyZens <roncint61@gmail.com>
2025-03-19 11:57:22 +0100

    main.h

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
+#include <stddef.h>
+
+int _putchar(char c);
+
+
+#endif /* VARIADIC_FUNCTIONS_H */
