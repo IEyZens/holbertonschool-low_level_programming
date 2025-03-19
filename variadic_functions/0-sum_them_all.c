ff748a44f5ae696dc231475f8b5bd5b8d480f394
IEyZens <roncint61@gmail.com>
2025-03-19 12:17:02 +0100

    sum_them_all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,15 +1,23 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - check the code
- *
+ * @n: est un int non assigné const
+ * Return: valid the code
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 1;
+	va_list args;
+	int result = 0;
+	unsigned int i;
 
 	if (n == 0)
 		return (0);
 
-	return (sum + n);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		result += va_arg(args, int);
+
+
+	return (result);
 }
