ad17badb6845399b0150127dc9240b472caa8fa3
IEyZens <roncint61@gmail.com>
2025-03-20 13:26:16 +0100

    print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,36 +1,84 @@
 #include "variadic_functions.h"
+
+void print_c(va_list c);
+void print_i(va_list i);
+void print_f(va_list f);
+void print_s(va_list s);
+
 /**
- *
- *
- */
+ * print_all - check the code
+ * @format: est un const char constant
+ **/
 void print_all(const char * const format, ...)
 {
+	format_t str_arr[] = {
+		{'c', print_c},
+		{'i', print_i},
+		{'f', print_f},
+		{'s', print_s},
+		{'\0', '\0'}
+	};
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *separator = "";
 	va_list args;
-	int len = 0;
-	char *s, *separator = "";
 
 	va_start(args, format);
-	while (format && format[len])
+	while (format && format[i])
 	{
-		if (format[len] == 'c')
-			printf("%s%c", separator, va_arg(args, int));
-		else if (format[len] == 'i')
-			printf("%s%d", separator, va_arg(args, int));
-		else if (format[len] == 'f')
-			printf("%s%f", separator, va_arg(args, double));
-		else if (format[len] == 's')
+		j = 0;
+		while (str_arr[j].format_in != '\0')
 		{
-			s = va_arg(args, char *);
-			if (!s)
-				s = "(nil)";
-			printf("%s%s", separator, s);
+			if (str_arr[j].format_in == format[i])
+			{
+				printf("%s", separator);
+				str_arr[j].function(args);
+				separator = ", ";
+				break;
+			}
+			j++;
 		}
-
-		separator = ", ";
-		len++;
+		i++;
 	}
-
 	printf("\n");
+}
 
-	va_end(args);
+/**
+ * print_c - print with format char.
+ * @c: char to print.
+ **/
+void print_c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_i - print with format int.
+ * @i: int to print.
+ **/
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - print with format float.
+ * @f: float to print.
+ **/
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_s - print with format string.
+ * @s: string to print.
+ **/
+void print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
 }
