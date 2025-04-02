215c1925cca5a7c20b42fd9f735e81b6c181eab4
IEyZens <roncint61@gmail.com>
2025-04-02 11:17:50 +0200

    Add commentary for betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 
 /**
+ * add_dnodeint - check the code
+ * @head: est un double pointeur
+ * @n: est un int constant
  *
+ * Return: valid the code
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
