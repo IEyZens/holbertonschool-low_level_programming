f2aff25a6672a2c313f520fabe1054882c44231a
IEyZens <roncint61@gmail.com>
2025-04-03 13:45:55 +0200

    Add function and library for project

---
+++ file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif /* MAIN_H */
