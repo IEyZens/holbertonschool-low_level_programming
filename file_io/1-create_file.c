c72e16ba63e42e76a09bd5022e3eb3c9fa321e79
IEyZens <roncint61@gmail.com>
2025-04-03 14:05:49 +0200

    Fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int cf;
-	ssize_t bytesWrite;
+	int bytesWrite;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,13 +19,15 @@ int create_file(const char *filename, char *text_content)
 	if (cf == -1)
 		return (-1);
 
-	bytesWrite = write(cf, text_content, strlen(text_content));
 	if (text_content != NULL)
+	{
+		bytesWrite = write(cf, text_content, strlen(text_content));
 		if (bytesWrite == -1)
 		{
 			close(cf);
 			return (-1);
 		}
+	}
 
 	close(cf);
 	return (1);
